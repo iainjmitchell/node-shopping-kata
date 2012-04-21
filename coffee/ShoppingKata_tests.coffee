da69f9f2e42e7d811510017c3cf6c74e8fe85830
Iain J Mitchell <iainjmitchell@gmail.com>
2012-04-21 15:03:28 +0100

    standard totals - refactored

--- coffee/ShoppingKata_tests.coffee
+++ coffee/ShoppingKata_tests.coffee
@@ -1,30 +1,85 @@
 vows = require 'vows'
 assert = require 'assert'
+EventEmitter = require('events').EventEmitter
+
+prices = 
+	'A': 50
+	'B': 30
+	'C': 20
+	'D': 15
 
 vows.describe('checkout tests')
 	.addBatch(
-		'no special offers':
+		'no special offers - one item':
 			'when one A': 
 				topic: () -> 
-					new Checkout(@callback, 50).scan('A').total()
+					new Checkout(@callback, prices).scan('A').total()
 					undefined
 				'total is 50': (err, total) ->
 					assert.equal total, 50
 			'when one B':
 				topic: () ->
-					new Checkout(@callback, 30).scan('B').total()
+					new Checkout(@callback, prices).scan('B').total()
 					undefined
 				'total is 30': (err, total) ->
 					assert.equal total, 30
+			'when one C':
+				topic: () ->
+					new Checkout(@callback, prices).scan('C').total()
+					undefined
+				'total is 30': (err, total) ->
+					assert.equal total, 20
+			'when one D':
+				topic: () ->
+					new Checkout(@callback, prices).scan('D').total()
+					undefined
+				'total is 30': (err, total) ->
+					assert.equal total, 15
+		'no special offers - multiple items':
+			'when two A':
+				topic: () -> 
+					new Checkout(@callback, prices).scan('A').scan('A').total()
+					undefined
+				'total is 100': (err, total) ->
+					assert.equal total, 100
+			'when one A and one B':
+				topic: () -> 
+					new Checkout(@callback, prices).scan('A').scan('B').total()
+					undefined
+				'total is 80': (err, total) ->
+					assert.equal total, 80
 	)
 	.run()
 
 
 class Checkout 
-	constructor: (@onTotal, @price) ->
-	scan: () -> this
-	total: () ->
-		console.log @onTotal
-		@onTotal undefined, @price 
+	constructor: (onTotal, @prices) ->
+		totaller = new Totaller(onTotal)
+		@scanner = new Scanner(@prices)
+		@scanner.on 'NewItem', (itemInfo) ->
+			totaller.add(itemInfo.price)
+		@totaller1 = totaller
 
+	scan: (item) -> 
+		@scanner.scan(item)
+		this
+	total: () ->
+		@totaller1.total()
+
+
+class Scanner extends EventEmitter
+	constructor: (@prices) ->
+	scan: (item) ->
+		@emit('NewItem',
+			item: item 
+			price: @prices[item]
+		)
+
+class Totaller 	
+	constructor: (@onTotal) ->
+		@totalAmount = 0
+	add: (price) ->
+		@totalAmount += price 
+	total: ()->
+		@onTotal undefined, @totalAmount
 
--- src/ShoppingKata_tests.js
+++ src/ShoppingKata_tests.js
@@ -1,15 +1,26 @@
 (function() {
-  var Checkout, assert, vows;
+  var Checkout, EventEmitter, Scanner, Totaller, assert, prices, vows,
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
   vows = require('vows');
 
   assert = require('assert');
 
+  EventEmitter = require('events').EventEmitter;
+
+  prices = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15
+  };
+
   vows.describe('checkout tests').addBatch({
-    'no special offers': {
+    'no special offers - one item': {
       'when one A': {
         topic: function() {
-          new Checkout(this.callback, 50).scan('A').total();
+          new Checkout(this.callback, prices).scan('A').total();
           return;
         },
         'total is 50': function(err, total) {
@@ -18,11 +29,49 @@
       },
       'when one B': {
         topic: function() {
-          new Checkout(this.callback, 30).scan('B').total();
+          new Checkout(this.callback, prices).scan('B').total();
           return;
         },
         'total is 30': function(err, total) {
           return assert.equal(total, 30);
+        }
+      },
+      'when one C': {
+        topic: function() {
+          new Checkout(this.callback, prices).scan('C').total();
+          return;
+        },
+        'total is 30': function(err, total) {
+          return assert.equal(total, 20);
+        }
+      },
+      'when one D': {
+        topic: function() {
+          new Checkout(this.callback, prices).scan('D').total();
+          return;
+        },
+        'total is 30': function(err, total) {
+          return assert.equal(total, 15);
+        }
+      }
+    },
+    'no special offers - multiple items': {
+      'when two A': {
+        topic: function() {
+          new Checkout(this.callback, prices).scan('A').scan('A').total();
+          return;
+        },
+        'total is 100': function(err, total) {
+          return assert.equal(total, 100);
+        }
+      },
+      'when one A and one B': {
+        topic: function() {
+          new Checkout(this.callback, prices).scan('A').scan('B').total();
+          return;
+        },
+        'total is 80': function(err, total) {
+          return assert.equal(total, 80);
         }
       }
     }
@@ -30,21 +79,65 @@
 
   Checkout = (function() {
 
-    function Checkout(onTotal, price) {
-      this.onTotal = onTotal;
-      this.price = price;
+    function Checkout(onTotal, prices) {
+      var totaller;
+      this.prices = prices;
+      totaller = new Totaller(onTotal);
+      this.scanner = new Scanner(this.prices);
+      this.scanner.on('NewItem', function(itemInfo) {
+        return totaller.add(itemInfo.price);
+      });
+      this.totaller1 = totaller;
     }
 
-    Checkout.prototype.scan = function() {
+    Checkout.prototype.scan = function(item) {
+      this.scanner.scan(item);
       return this;
     };
 
     Checkout.prototype.total = function() {
-      console.log(this.onTotal);
-      return this.onTotal(void 0, this.price);
+      return this.totaller1.total();
     };
 
     return Checkout;
+
+  })();
+
+  Scanner = (function(_super) {
+
+    __extends(Scanner, _super);
+
+    function Scanner(prices) {
+      this.prices = prices;
+    }
+
+    Scanner.prototype.scan = function(item) {
+      return this.emit('NewItem', {
+        item: item,
+        price: this.prices[item]
+      });
+    };
+
+    return Scanner;
+
+  })(EventEmitter);
+
+  Totaller = (function() {
+
+    function Totaller(onTotal) {
+      this.onTotal = onTotal;
+      this.totalAmount = 0;
+    }
+
+    Totaller.prototype.add = function(price) {
+      return this.totalAmount += price;
+    };
+
+    Totaller.prototype.total = function() {
+      return this.onTotal(void 0, this.totalAmount);
+    };
+
+    return Totaller;
 
   })();
 
